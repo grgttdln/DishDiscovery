3eafe9185500363b5d7e0ac1b58b349a44414848
grgttdln <georgettedalen@gmail.com>
2024-03-31 11:40:14 +0800

    Bookmark Main Content Page Added

--- components/Search.js
+++ components/Search.js
@@ -4,14 +4,22 @@ import {
   FlatList,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons/faBookmark";
 import search from "../styles/SearchStyles";
 
 const Search = () => {
   return (
     <View>
-      <Text style={search.title}>What would you like to cook today?</Text>
+      <View style={search.subContainer}>
+        <Text style={search.title}>What would you like to cook today?</Text>
+        <TouchableOpacity style={search.circle}>
+          <FontAwesomeIcon icon={faBookmark} size={20} />
+        </TouchableOpacity>
+      </View>
       <TextInput style={search.container} placeholder="Search for Recipes" />
     </View>
   );
--- styles/SearchStyles.js
+++ styles/SearchStyles.js
@@ -10,10 +10,25 @@ const search = StyleSheet.create({
   },
 
   title: {
-    fontSize: 22,
+    fontSize: 24,
     fontWeight: "700",
     marginVertical: 5,
     marginRight: 60,
+  },
+
+  subContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+
+  circle: {
+    width: 43,
+    height: 43,
+    borderRadius: 25,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
